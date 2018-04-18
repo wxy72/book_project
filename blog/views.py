95ec66f2fbeee495cd2baa632d4a8f3ff332f45e
wxy72 <18227109996@163.com>
2018-04-18 21:56:00 +0800

    准备在模板里渲染数据

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import BlogArticles
 
 # Create your views here.
+def blog_title(request):
+	blogs = BlogArticles.objects.all()
+	return render(request,'blog/title.html',{'blogs':blogs})
